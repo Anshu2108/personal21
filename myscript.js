10e1d0917422a10a4567b8a358c5dfc8f873f9af
Anshu2108 <110243866+Anshu2108@users.noreply.github.com>
2023-06-30 11:47:51 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+  // Open the side bar
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+// Close the side bar
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
